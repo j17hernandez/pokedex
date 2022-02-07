4eb8b4b3397fee4a269f50db13a9e28d0f8152e9
Jorge Hernandez <hernandezrayojorge@gmail.com>
2022-02-07 15:42:07 -0500

    My first commit

--- README.md
+++ README.md
@@ -1,24 +1,57 @@
 # pokedex
 
+Proyecto de Vue Js, implementando la API de [Pokemon](https://pokeapi.co/)
+
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
+## Special Directories
+
+### `assets`
+
+The assets directory contains your uncompiled assets such as Stylus or Sass files, images, or fonts.
+
+### `components`
+
+The components directory contains your Vue.js components. Components make up the different parts of your page and can be reused and imported into your pages, layouts and even other components.
+
+### `plugins`
+
+The plugins directory contains JavaScript plugins that you want to run before instantiating the root Vue.js Application. This is the place to add Vue plugins and to inject functions or constants. Every time you need to use `Vue.use()`, you should create a file in `plugins/` and add its path to plugins in `src/main.js`.
+
+### `router`
+
+The router directory contains your application routes, these routes are from the views and each needs to be imported.
+
+### `store`
+
+This directory contains your Vuex store files. Creating a file in this directory automatically activates Vuex.
+
+### `views`
+
+This directory contains your application views
+
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/Home.vue
@@ -0,0 +1,110 @@
+<template>
+  <v-container fluid>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <div height="200" width="200" class="contenedor">
+          <div style="position: absolute">
+            <img :src="require('../assets/Ellipse10.png')" alt="" class="eye" />
+            <img
+              :src="require('../assets/pikachu.png')"
+              alt=""
+              class="heaven"
+            />
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <h1 class="">Welcome to Pokédex</h1>
+        <p class="">
+          The digital encyclopedia created by Professor Oak is an invaluable
+          tool to Trainers in the Pokémon world.
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: "HomeComponent",
+
+    data: () => ({}),
+  };
+</script>
+<style scoped>
+  .title {
+    position: absolute;
+    width: 570px;
+    height: 31px;
+    left: 291px;
+    top: 418px;
+    font-family: Lato;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 26px;
+    line-height: 31px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    color: #353535;
+  }
+  .subtitle {
+    position: absolute;
+    width: 570px;
+    height: 54px;
+    left: 291px;
+    top: 479px;
+    font-family: Lato;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 150%;
+    /* or 27px */
+    text-align: center;
+    color: #5e5e5e;
+  }
+  .eye {
+    position: absolute;
+    /* height: 170px;
+    width: 170px; */
+    top: 58px;
+    z-index: 0;
+  }
+
+  .heaven {
+    position: absolute;
+    z-index: 1;
+    transform: translate(-46px, -7px);
+
+    /* position: absolute;
+    height: 230px;
+    width: 248px;
+    z-index: 1;
+    transform: translate(-33px, -7px); */
+  }
+  .contenedor {
+    display: inline-flex;
+    transform: translate(-58px, 10px);
+    line-height: normal;
+    position: relative;
+    text-align: center;
+    vertical-align: bottom;
+  }
+  @media (max-width: 575.98px) {
+    .heaven {
+      position: absolute;
+      width: 700px;
+      z-index: 1;
+      transform: translate(-81px, -41px);
+    }
+    .eye {
+      position: absolute;
+      /* height: 170px; */
+      width: 500px;
+      top: 58px;
+      z-index: 0;
+    }
+  }
+</style>
